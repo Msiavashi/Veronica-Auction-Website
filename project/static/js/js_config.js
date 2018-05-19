c84748b1506d810680e4bf9c85dd78777d374d80
mr_asadi <mr.asadi1987@gmail.com>
2018-05-19 19:12:02 +0430

    adding js config for production websocket port settings

---
+++ project/static/js/js_config.js
@@ -0,0 +1,8 @@
+if (window.location.protocol == "https:") {
+    var ws_scheme = "wss://";
+    }
+  else {
+    var ws_scheme = "ws://"
+};
+var SERVERSOCKET_RECEIVE = ws_scheme + location.host + "/receive";
+var SERVERSOCKET_SUBMIT = ws_scheme + location.host + "/submit";
