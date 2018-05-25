8d7ffc770fb6ebc2a0a3c9186932a90393cd0432
mr_asadi <mr.asadi1987@gmail.com>
2018-05-25 08:11:34 +0430

    adding socketio

--- runserver.py
+++ runserver.py
@@ -7,6 +7,6 @@ from project.admin import admin
 import definitions
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     # app.run('0.0.0.0', port=port, threaded=True)
     socketio.run(app, port=port, debug=True)
