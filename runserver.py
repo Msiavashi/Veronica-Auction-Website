404ab64e6179553ee3fcd4894b58a64be8f0ec15
mr_asadi <mr.asadi1987@gmail.com>
2018-06-29 21:16:03 +0430

    changes for test server

--- runserver.py
+++ runserver.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     # app.run('0.0.0.0', port=port, threaded=True)
     socketio.run(app, port=port, debug=True)
-    #for test commit
+    
