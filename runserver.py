f3b5db1124ac4ed7c78ab132207439f9fe1740cf
mr_asadi <mr.asadi1987@gmail.com>
2018-10-11 10:09:51 +0330

    ignoring runserver for production

--- runserver.py
+++ runserver.py
@@ -6,6 +6,12 @@ from project.database import *
 from project.admin import admin
 
 if __name__ == '__main__':
+    # production
     port = int(os.environ.get("PORT", 8000))
-    # app.run('0.0.0.0', port=port, threaded=True)
+    app.debug = False
     socketio.run(app, port=port, debug=False)
+
+    # developement
+    # port = int(os.environ.get("PORT", 9000))
+    # app.debug = True
+    # socketio.run(app, port=port, debug=True)
